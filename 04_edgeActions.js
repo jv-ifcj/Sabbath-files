5ca1d9d742147cc6685d0f6823837a1bf6cc9e13
mitchajohnson <mjo3689@gmail.com>
2012-12-03 02:13:32 -0600

    Partial functionality for keyboard navigation for 04 - Mitch

--- 04_edgeActions.js
+++ 04_edgeActions.js
@@ -66,28 +66,30 @@ Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 17500, function
 $("#Stage_submit_focus").hide();
 $("#Stage_q2_skip_button")
 	//.focus()
-	.attr("tabindex", "1");
+	//.attr("tabindex", "1");
 //$("#Stage_q1_submit_button").attr("tabindex", "2");
 
 //if (!$("#Stage_q1_submit_button").focus) {
-if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").css("display") != "none") {
 //if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").css("display") != "none") {
-	$("#Stage_q1_submit_button")
-		.unbind("focus")
-		.focus(function() {
-		$("#Stage_submit_focus").show();
+//if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").css("display") != "none") {
+//	$("#Stage_q1_submit_button")
+//		.unbind("focus")
+		//.focus(function() {
+		//$("#Stage_submit_focus").show();
 			//Un-comment line 11 ONLY
-	});
-	}
+			;
+	//});
+	//}
 //}
 
 //if (!$("#Stage_q1_submit_button").blur) {
-	$("#Stage_q1_submit_button")
-		.unbind("blur")
-		.blur(function() {
-			$("#Stage_submit_focus").hide();
-	});
+//	$("#Stage_q1_submit_button")
+//		.unbind("blur")
+//		.blur(function() {
+//			$("#Stage_submit_focus").hide();
+//	});
 //}
+
 
 });
 //Edge binding end
@@ -97,7 +99,14 @@ Symbol.bindElementAction(compId, symbolName, "${_NextButton}", "click", function
 //Edge binding end
 Symbol.bindElementAction(compId, symbolName, "${_A_timeline_button}", "click", function(sym,e){sym.play("a_select");});
 //Edge binding end
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 17750, function(sym,e){sym.stop();});
+Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 17750, function(sym,e){sym.stop();
+$("#Stage_a_submit_button")
+		.unbind("focus")
+		.focus(function() {
+		$("#Stage_submit_focus").show();
+				});
+
+});
 //Edge binding end
 Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 19750, function(sym,e){sym.stop();});
 //Edge binding end
@@ -187,6 +196,29 @@ Symbol.bindElementAction(compId, symbolName, "${_sb09}", "click", function(sym,e
 //Edge binding end
 Symbol.bindElementAction(compId, symbolName, "${_q4_skip_go_button}", "click", function(sym,e){sym.play("q4_skip_go");});
 //Edge binding end
+      
+
+      
+
+      Symbol.bindElementAction(compId, symbolName, "${_q1_submit_button}", "focus", function(sym, e) {
+         //$("#Stage_submit_focus").show(); // Show an Element.
+         //  (sym.$("name") resolves an Edge Animate element name to a DOM
+         //  element that can be used with jQuery)
+         //sym.$("submit_focus").show();
+         
+
+      });
+      //Edge binding end
+
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 14500, function(sym, e) {
+         $("#Stage_q1_submit_button")
+         		.unbind("focus");
+         $("#Stage_q1_skip_button").unbind("focus");
+         $("#Stage_q1_skip_go_button").unbind("focus");
+
+      });
+      //Edge binding end
+
       Symbol.bindElementAction(compId, symbolName, "document", "keydown", function(sym, e) {
          // insert code to be run when the user presses a key.
          // The variable “e.which” tells you the key code of the key that was pressed, e.g. 32 = space
@@ -196,9 +228,44 @@ Symbol.bindElementAction(compId, symbolName, "${_q4_skip_go_button}", "click", f
          	}
          	else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
          		sym.play("q1_next_go");
-         		//script here must match the onclick script (open actions) on object q1_submit_button
-         		//That script will include the script to save answer to database.
+         		//Copy object's onclick script here for submitting answer
          	}
+         
+         	//Uncomment one section at a time below as you need more enter key keydown functions
+         	else if ($("#Stage_a_submit_button") && $("#Stage_a_submit_button").css("display") != "none") {
+         		sym.play("a_submit");
+         		//Copy object's onclick script here for submitting answer #answersubmit a_submit_button
+         	}
+         
+         	else if ($("#Stage_q2_b_submit_button") && $("#Stage_q2_b_submit_button").css("display") != "none") {
+         		sym.play("b_submit");
+         		//Copy object's onclick script here for submitting answer #answersubmit q2_b_submit_button
+         	}
+         
+         	else if ($("#Stage_q2_c_submit_button") && $("#Stage_q2_c_submit_button").css("display") != "none") {
+         		sym.play("c_submit");
+         		//Copy object's onclick script here for submitting answer #answersubmit q2_c_submit_button
+         	}
+         	/*else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
+         		sym.play("q1_next_go");
+         		//Copy object's onclick script here for submitting answer
+         	}*/
+         	/*else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
+         		sym.play("q1_next_go");
+         		//Copy object's onclick script here for submitting answer
+         	}*/
+         	/*else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
+         		sym.play("q1_next_go");
+         		//Copy object's onclick script here for submitting answer
+         	}*/
+         	/*else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
+         		sym.play("q1_next_go");
+         		//Copy object's onclick script here for submitting answer
+         	}*/
+         	/*else if ($("#Stage_q1_submit_button") && $("#Stage_q1_submit_button").is(":focus") && $("#Stage_q1_submit_button").css("display") != "none") {
+         		sym.play("q1_next_go");
+         		//Copy object's onclick script here for submitting answer
+         	}*/
          	else if ($("#Stage_NextButton") && $("#Stage_NextButton").css("display") != "none") {
          		sym.play();
          		//Insert onclick script from object NextButton here.
@@ -212,6 +279,77 @@ Symbol.bindElementAction(compId, symbolName, "${_q4_skip_go_button}", "click", f
          	}
          }
          */
+         
+         //Section for down arrow
+         if (e.which == 40) {
+         	if($("#Stage_select_a_highlight") && $("#Stage_select_a_highlight").css("display") != "none") {
+         		sym.play('b_select');
+         	}
+         
+         	else if ($("#Stage_select_b_highlight") && $("#Stage_select_b_highlight").css("display") != "none")
+         		{
+         		sym.play('c_select');
+         		}
+         
+         	else if ($("#Stage_select_c_highlight") && $("#Stage_select_c_highlight").css("display") != "none")
+         		{
+         		sym.play('a_select');
+         		}	
+         
+         	else if ($("#Stage_Question2") && $("#Stage_Question2").css("display") != "none")
+         		{
+         		sym.play('a_select');
+         		}	
+         }
+         
+         //Section for up arrow
+         if (e.which == 38) {
+         	if($("#Stage_select_a_highlight") && $("#Stage_select_a_highlight").css("display") != "none") {
+         		sym.play('c_select');
+         	}
+         
+         	else if ($("#Stage_select_b_highlight") && $("#Stage_select_b_highlight").css("display") != "none")
+         		{
+         		sym.play('a_select');
+         		}
+         
+         	else if ($("#Stage_select_c_highlight") && $("#Stage_select_c_highlight").css("display") != "none")
+         		{
+         		sym.play('b_select');
+         		}	
+         
+         	else if ($("#Stage_Question2") && $("#Stage_Question2").css("display") != "none")
+         		{
+         		sym.play('c_select');
+         		}	
+         }
+         
+         //Section for tab key
+         if (e.which == 9) {
+         	if($("#Stage_select_a_highlight") && $("#Stage_select_a_highlight").css("display") != "none") {
+         		sym.play('b_select');
+         	}
+         
+         	else if ($("#Stage_select_b_highlight") && $("#Stage_select_b_highlight").css("display") != "none")
+         		{
+         		sym.play('c_select');
+         		}
+         
+         	else if ($("#Stage_select_c_highlight") && $("#Stage_select_c_highlight").css("display") != "none")
+         		{
+         		sym.play('q2_skip_check');
+         		}	
+         
+         	else if ($("#Stage_q2_skip_go_button") && $("#Stage_q2_skip_go_button").css("display") != "none")
+         		{
+         		sym.play('a_select');
+         		}	
+         
+         	else if ($("#Stage_Question2") && $("#Stage_Question2").css("display") != "none")
+         		{
+         		sym.play('a_select');
+         		}	
+         }
 
       });
       //Edge binding end
@@ -225,16 +363,6 @@ Symbol.bindElementAction(compId, symbolName, "${_q4_skip_go_button}", "click", f
          else {
          sym.getComposition().getStage().play();
          }
-
-      });
-      //Edge binding end
-
-      Symbol.bindElementAction(compId, symbolName, "${_q1_submit_button}", "focus", function(sym, e) {
-         //$("#Stage_submit_focus").show(); // Show an Element.
-         //  (sym.$("name") resolves an Edge Animate element name to a DOM
-         //  element that can be used with jQuery)
-         //sym.$("submit_focus").show();
-         
 
       });
       //Edge binding end
